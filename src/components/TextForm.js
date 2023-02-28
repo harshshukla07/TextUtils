1dabbdac76d2a449a64101369e2257ff93dafa25
Harsh Shukla <harshshukla5810@gmail.com>
2023-03-01 01:18:02 +0530

    now text to upper works

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
+    const handleUpClick = () => {
+        let newText = text.toUpperCase()
+        setText(newText)
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState("Enter text here");
     return (
         <div>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary">Convert to Upper case</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper case</button>
         </div>
     )
 }
