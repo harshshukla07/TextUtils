3d5981eba684016f47dbf13f60d02c81bdeb0344
Harsh Shukla <harshshukla5810@gmail.com>
2023-03-01 00:02:50 +0530

    added button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,7 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" id="myBox" rows="8"></textarea>
             </div>
+            <button className="btn btn-primary">Convert to Upper case</button>
         </div>
     )
 }
